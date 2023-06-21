367a6f89beb8cfb5ae799cfedcd36218d10306e8
Jairison Silva de Souza <116470586+jairisonsouza@users.noreply.github.com>
2023-06-21 09:10:50 -0300

    Add files via upload

--- src/componentes/ListaSuspensa/index.js
+++ src/componentes/ListaSuspensa/index.js
@@ -1,11 +1,11 @@
 import './ListaSuspensa.css'
 
 const ListaSuspensa = (props) => {
-    
     return (
         <div className='lista-suspensa'>
             <label>{props.label}</label>
-            <select onChange={evento => props.aoAlterado(evento.target.value)} required={props.required}>
+            <select onChange={evento => props.aoAlterado(evento.target.value)} required={props.required} value={props.valor}>
+                <option value=""></option>
                 {props.itens.map(item => {
                     return <option key={item}>{item}</option>
                 })}
